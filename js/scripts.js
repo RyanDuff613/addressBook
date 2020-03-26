f28268aacaea5e76559b086095d52710cb46dc92
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 23:45:13 -0700

    add js code to take in data from html form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 
+  $('#theForm').submit(function(event){
+    event.preventDefault();
+    
+    var name = $('#name').val();
+    var address1 = $('#address1').val();
+    var address2 = $('#address2').val();
+    var phone = $('#phone').val();
+    var email = $('#email').val();
+
+    $('#contactList').prepend();
+  });
+
+
 });
